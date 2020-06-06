c25a7bc6d2046b3fa7fefadb6b7ed13d244db6bd
Leo Brack <leo.brack@hotmail.co.uk>
2020-06-06 22:01:28 +0100

    Tutorial 2: Building Lists and Navigation

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Leo brack on 06/06/2020.
+//  Copyright © 2020 Leo brack. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
